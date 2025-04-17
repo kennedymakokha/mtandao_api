db362f5ebc76d5a94bf171c62a2d1e9f73136e7e
kennedymakokha <katchibo2@gmail.com>
2025-04-17 10:58:29 +0300

    fixing the PORT

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://mar
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 connectDB();
 
 const httpServer = createServer(app);
